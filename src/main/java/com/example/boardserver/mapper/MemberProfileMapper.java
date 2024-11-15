3c5fc27a00f4727320751000de70996a9947aae0
fullm <fullmoon0207@naver.com>
2024-11-15 23:55:12 +0900

    enh: Member 관련 mapper

---
+++ src/main/java/com/example/boardserver/mapper/MemberProfileMapper.java
@@ -0,0 +1,31 @@
+package com.example.boardserver.mapper;
+
+import com.example.boardserver.dto.MemberDTO;
+import java.lang.reflect.Member;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface MemberProfileMapper {
+
+    public MemberDTO getMemberProfile(@Param("memberId") String memberId);
+
+    int insertMemberProfile(@Param("id") String id, @Param("password") String password,
+            @Param("name") String name, @Param("address") String address,
+            @Param("createTime") String createTime, @Param("updateTime") String updateTime);
+
+    int deleteMemberProfile(@Param("id") String id);
+
+    public int register(MemberDTO memberDTO);
+
+    public MemberDTO findByIdAndPassword(@Param("id") String id, @Param("password") String password);
+
+    public MemberDTO findByMemberIdAndPassword(@Param("memberId") String memberId,
+            @Param("password") String password);
+
+    int idCheck(@Param("id") String id);
+
+    public int updatePassword(MemberDTO memberDTO);
+
+    public int updateAddress(MemberDTO memberDTO);
+}
