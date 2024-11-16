aa7e0711b9f064d43a06d8585bd484e9d7382ece
fullm <fullmoon0207@naver.com>
2024-11-16 14:18:01 +0900

    enh: 카테고리 DTO

---
+++ src/main/java/com/example/boardserver/dto/CategoryDTO.java
@@ -0,0 +1,25 @@
+package com.example.boardserver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDTO {
+
+    private int id;
+    private String name;
+    private SortStatus sortStatus;
+    private int searchCount;
+    private int pagingStartOffset;
+
+    public enum SortStatus{
+        CATEGORIES, NEWEST, OLDEST, HIGHPRICE, LOWPRICE, GRADE
+    }
+}
