c43c9f574226443e4c9fa8fb00cee963186af693
fullm <fullmoon0207@naver.com>
2024-11-16 14:18:19 +0900

    enh: 카테고리 request

---
+++ src/main/java/com/example/boardserver/dto/request/CategoryRequest.java
@@ -0,0 +1,14 @@
+package com.example.boardserver.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CategoryRequest {
+
+    private int id;
+
+    private String name;
+
+}
