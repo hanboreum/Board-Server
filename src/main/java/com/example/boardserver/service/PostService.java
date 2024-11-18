d2d798f9c28090675dc2098f3ece32e66b839634
fullm <fullmoon0207@naver.com>
2024-11-18 21:41:21 +0900

    enh: 태그, 댓글 서비스

--- src/main/java/com/example/boardserver/service/PostService.java
+++ src/main/java/com/example/boardserver/service/PostService.java
@@ -1,6 +1,8 @@
 package com.example.boardserver.service;
 
+import com.example.boardserver.dto.CommentDTO;
 import com.example.boardserver.dto.PostDTO;
+import com.example.boardserver.dto.TagDTO;
 import java.util.List;
 
 public interface PostService {
@@ -12,4 +14,16 @@ public interface PostService {
     void updateProducts(PostDTO dto);
 
     void deleteProduct(int memberId, int productId);
+
+    void registerComment(CommentDTO dto);
+
+    void updateComment(CommentDTO dto);
+
+    void deleteComment(int memberId, int commentId);
+
+    void registerTag(TagDTO dto);
+
+    void updateTag(TagDTO dto);
+
+    void deletePostTag(int memberId, int tagId);
 }
