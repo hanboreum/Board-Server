4ddcdab3d55ceb48d38075dc8873d722dbf903a2
fullm <fullmoon0207@naver.com>
2024-11-18 21:42:19 +0900

    enh: 태그 mapper

---
+++ src/main/java/com/example/boardserver/mapper/TagMapper.java
@@ -0,0 +1,17 @@
+package com.example.boardserver.mapper;
+
+import com.example.boardserver.dto.TagDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TagMapper {
+
+    public int register(TagDTO dto);
+
+    public void updateTags(TagDTO dto);
+
+    public void deletePostTag(int postId);
+
+    //태그와 게시글의 n:n 관계를 표시하기 위한 post tag table 에 정보를 저장하기 위함
+    public void createPostTag(Integer tagId, Integer postId); //Integer?
+}
