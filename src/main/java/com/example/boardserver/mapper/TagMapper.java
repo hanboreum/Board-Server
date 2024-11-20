94bdf6dc26ab79f894501b777999b74be96797c2
fullm <fullmoon0207@naver.com>
2024-11-20 15:43:09 +0900

    docs: 주석 추가

--- src/main/java/com/example/boardserver/mapper/TagMapper.java
+++ src/main/java/com/example/boardserver/mapper/TagMapper.java
@@ -12,6 +12,6 @@ public interface TagMapper {
 
     public void deletePostTag(int postId);
 
-    //태그와 게시글의 n:n 관계를 표시하기 위한 post tag table 에 정보를 저장하기 위함
+    //태그와 게시글의 m:n 관계를 표시하기 위한 post tag table 에 정보를 저장하기 위함
     public void createPostTag(Integer tagId, Integer postId); //Integer?
 }
